8924021ba5b022f2c6b3662a69f480c8df739efe
Adrian Lee <alee@xmatters.com>
2019-07-11 10:03:27 -0700

    initial commit

--- src/main/java/com/battlesnake/RequestController.java
+++ src/main/java/com/battlesnake/RequestController.java
@@ -23,6 +23,8 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class RequestController {
 
+
+
     @RequestMapping(value="/start", method=RequestMethod.POST, produces="application/json")
     public StartResponse start(@RequestBody StartRequest request) {
         return new StartResponse()
@@ -39,14 +41,26 @@ public class RequestController {
         MoveResponse moveResponse = new MoveResponse();
         
         Snake mySnake = findOurSnake(request); // kind of handy to have our snake at this level
-        
-        List<Move> towardsFoodMoves = moveTowardsFood(request, mySnake.getCoords()[0]);
-        
-        if (towardsFoodMoves != null && !towardsFoodMoves.isEmpty()) {
-            return moveResponse.setMove(towardsFoodMoves.get(0)).setTaunt("I'm hungry");
-        } else {
-            return moveResponse.setMove(Move.DOWN).setTaunt("Oh Drat");
+        int[] head = mySnake.getCoords()[0];
+
+        int[][] map = getMap(request);
+
+        Move theMove = Move.UP;
+        for (Move move : Move.values()) {
+            if (moveIsOk(map, head, move)) {
+                theMove = move;
+                break;
+            }
         }
+        
+//        List<Move> towardsFoodMoves = moveTowardsFood(request, mySnake.getCoords()[0]);
+        
+//        if (towardsFoodMoves != null && !towardsFoodMoves.isEmpty()) {
+//            return moveResponse.setMove(towardsFoodMoves.get(0)).setTaunt("I'm hungry");
+//        } else {
+//            return moveResponse.setMove(Move.DOWN).setTaunt("Oh Drat");
+//        }
+            return moveResponse.setMove(theMove).setTaunt("???");
     }
 
     @RequestMapping(value="/end", method=RequestMethod.POST)
@@ -54,6 +68,39 @@ public class RequestController {
         // No response required
         Map<String, Object> responseObject = new HashMap<String, Object>();
         return responseObject;
+    }
+
+    int[][] getMap(MoveRequest request) {
+        int[][] map = new int[request.getWidth()][request.getHeight()];
+        for (int x = 0; x < request.getWidth(); x++) {
+            for (int y = 0; y < request.getHeight(); y++) {
+                map[x][y] = 1;
+            }
+        }
+
+        // Don't hit snakes
+        for (Snake snake : request.getSnakes()) {
+            for (int i = 0; i < snake.getCoords().length; i++) {
+                int[] p = snake.getCoords()[i];
+                map[p[0]][p[1]] = 0;
+            }
+        }
+
+        return map;
+    }
+
+    boolean moveIsOk(int[][] map, int[] head, Move move) {
+        if (move == Move.LEFT) {
+            return head[0] > 0 && map[head[0] - 1][head[1]] > 0;
+        }
+        if (move == Move.RIGHT) {
+            return head[0] < map.length - 1 && map[head[0] + 1][head[1]] > 0;
+        }
+        if (move == Move.DOWN) {
+            return head[1] > map[0].length - 1 && map[head[0]][head[1] + 1] > 0;
+        }
+        // UP
+            return head[1] > 0 && map[head[0]][head[1] - 1] > 0;
     }
 
     /*
@@ -67,7 +114,6 @@ public class RequestController {
         List<Snake> snakes = request.getSnakes();
         return snakes.stream().filter(thisSnake -> thisSnake.getId().equals(myUuid)).findFirst().orElse(null);
     }
-
 
     /*
      *  Simple algorithm to find food
